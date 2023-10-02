4a8e11b69ec9c1ca1b9b82225b911a7ea4aea81d
jayachandra9912 <97786283+jayachandra9912@users.noreply.github.com>
2023-10-02 19:10:50 +0530

    Updates

---
+++ precache-manifest.081b098e35d028b35a3ebcd9aa5f4314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eda86c3fa77cd76a57d5",
+    "url": "/firstReactApp/static/css/main.f262bcef.chunk.css"
+  },
+  {
+    "revision": "eda86c3fa77cd76a57d5",
+    "url": "/firstReactApp/static/js/main.eda86c3f.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/firstReactApp/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/firstReactApp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "a8358b5f63df5143e6f7",
+    "url": "/firstReactApp/static/js/runtime~main.a8358b5f.js"
+  },
+  {
+    "revision": "fc8dbdf35a80183889c315239d906b71",
+    "url": "/firstReactApp/index.html"
+  }
+];
